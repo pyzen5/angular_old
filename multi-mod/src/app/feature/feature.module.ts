fefe4587a04395130e62dbc4b0b899d30957c5b0
Pyzen <pyzen55@gmail.com>
2017-09-19 10:52:31 +0530

    Create feature.module.ts

---
+++ multi-mod/src/app/feature/feature.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FeatureComponent } from './feature.component';
+
+@NgModule({
+    declarations:[FeatureComponent],
+    exports:[FeatureComponent]
+})
+export class featureModule{}
